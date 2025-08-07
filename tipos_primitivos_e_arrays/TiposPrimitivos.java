c8f6f3b8549f9f3a50e810bc880556aa77aeb5c0
pedroleonez <pedroleonez1@gmail.com>
2025-08-07 13:12:39 -0300

    feat: create and impl TiposPrimitivos

---
+++ tipos_primitivos_e_arrays/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package tipos_primitivos_e_arrays;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int inteiro = 10; // 32 bits
+        double decimal = 4.6; // 64 bits
+        float flutuante = 3.8f; // 32 bits
+        long longo = 88888L; // inteiro longo - 64 bits
+        short curto = 55; // inteiro curto - 16 bits
+        byte bite = 4; // inteiro pequeno - 8 bits
+        char caractere = 'P';
+        boolean ok = true;
+
+        System.out.println(inteiro);
+        System.out.println(decimal);
+        System.out.println(flutuante);
+        System.out.println(longo);
+        System.out.println(curto);
+        System.out.println(bite);
+        System.out.println(caractere);
+        System.out.println(ok);
+    }
+}
